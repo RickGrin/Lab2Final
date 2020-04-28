fb1390b28f60af0d34f3ac786ae33b719de0544e
Сергей <56985915+RickGrin@users.noreply.github.com>
2020-04-28 08:29:43 +0700

    strel.final

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,65 +1,74 @@
-﻿#include <iostream>
-
-#include <vector>
-
-#include <map>
-
-#include <cmath>
-
-#include <queue>
-
-#include <set>
-
-#include <iomanip>
-
-#include <map>
-
+﻿
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
 using namespace std;
 
+void rezultat(int** n, int* n1, int a, int b);
+void pobeda(int* n, int a);
 
-signed main() {
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    int n, m;
+    cout << "vvedite kol-vo strelkov-";
+    cin >> n;
+    cout << "vvedite kol-vo vistrelov-";
+    cin >> m;
+    while (n == 0 || m == 0 || n == 1 || m == 1);
+    int** a;
+    a = new int* [n];
+    for (int i = 0; i < n; i++)
+        a[i] = new int[m];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            a[i][j] = rand() % 11;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%5d", a[i][j]);
+        }
+        cout << endl;
+    }
+    int* result;
+    result = new int[n];
+    rezultat(a, result, n, m);
+    pobeda(result, n);
+    return 0;
+}
+void rezultat(int** n, int* n1, int a, int b)
+{
+    int s;
+    for (int i = 0; i < a; i++)
+    {
+        s = 0;
+        for (int j = 0; j < b; j++)
+            s += n[i][j];
+        n1[i] = s;
+    }
+    cout << "rezul'tat: " << endl;
+    for (int i = 0; i < a; i++)
 
-	ios_base::sync_with_stdio(0);
-
-	cin.tie(0);
-
-	cout.tie(0);
-
-	int n, m;
-
-	cin >> n >> m;
-
-	int max_val = -1, pointer = -1;
-
-	for (int i = 0; i < n; i++)
-
-	{
-
-		int sum = 0, k;
-
-		for (int j = 0; j < m; j++)
-
-		{
-
-			cin >> k;
-
-			sum += k;
-
-		}
-
-		if (sum > max_val)
-
-		{
-
-			max_val = sum;
-
-			pointer = i;
-
-		}
-
-	}
-
-	cout << max_val << "\n" << pointer;
-
+        cout << n1[i] << endl;
 }
 
+void pobeda(int* n, int a)
+{
+    int max;
+    max = n[0];
+    for (int i = 0; i < a; i++)
+    {
+        if (n[i] > max)
+            max = n[i];
+    }
+    cout << "max result = " << max << endl;
+    for (int i = 0; i < a; i++)
+    {
+        if (n[i] == max)
+        {
+            cout << "pobejdaet № " << i + 1 << endl;
+        }
+    }
+}
